48503db6bd5ba665dea93d0caaf9255df8fc0f7d
Alexandre Gutierrez <gutierreztransporte@gmail.com>
2023-02-18 00:38:46 +0000

    first commit

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      
+
+      <Image style={{width:100,height:100}} source={require('./assets/danki.png')} />
+      
+       
+       
+
+      
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
